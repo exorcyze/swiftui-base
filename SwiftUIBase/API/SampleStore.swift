3e7b922167543ec4044be4eb3ac8395615301ccc
exorcyze <rangdo2003@yahoo.com>
2025-03-10 11:15:36 -0700

    Small updates to sample store

--- SwiftUIBase/API/SampleStore.swift
+++ SwiftUIBase/API/SampleStore.swift
@@ -16,23 +16,27 @@ class SampleDataStore : ObservableObject {
     func getGithubUser() async throws -> GithubUser {
         let endpoint = "https://api.github.com/users/exorcyze"
         return try await network.perform( url: endpoint )
+        
+        // if additional things needed, such as dealing with a wrapper return packet type
+        // that may not match the function return type
         //let response : GithubUser = try await perform( url: endpoint )
         //return response
     }
     
     func postData() async throws -> GithubUser {
-        let endpoint = "https://api.github.com/users/exorcyze"
+        let endpoint = "https://someendpoint.com"
         guard let url = URL( string: endpoint ) else { throw AppError.invalidUrl }
         var request = URLRequest( url: url )
         request.httpMethod = "POST"
         request.setValue( "application/json", forHTTPHeaderField: "Content-Type" )
         
         let postData = PostData( name: "Count Chocula", age: 48 )
-        let jsonData = try JSONEncoder().encode(postData)
+        let jsonData = try JSONEncoder().encode( postData )
         request.httpBody = jsonData
 
         return try await network.perform( request: request )
     }
+    
     struct PostData : Codable {
         let name : String
         let age : Int
