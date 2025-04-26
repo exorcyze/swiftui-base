29f843e4f1a330654178f2dd9ddd1337d41e1631
exorcyze <rangdo2003@yahoo.com>
2025-04-26 08:28:34 -0700

    Add bindable environment object

--- SwiftUIBase/SwiftUIBaseApp.swift
+++ SwiftUIBase/SwiftUIBaseApp.swift
@@ -9,11 +9,38 @@ import SwiftUI
 
 @main
 struct SwiftUIBaseApp: App {
+
+    @State private var appController = AppController()
+    
     var body: some Scene {
         WindowGroup {
             Route( MainRoute.root )
             //ContentView()
+                .environment( appController )
                 .task { ProjectInfo.logInfo() }
+        }
+    }
+}
+
+
+@Observable
+class AppController {
+    var text = "Test"
+}
+
+struct AppControllerSampleView: View {
+    
+    @Environment(AppController.self) private var appController
+    
+    var body: some View {
+        // allow us to bind to it
+        @Bindable var appController = appController
+        
+        VStack {
+            Text( appController.text )
+            
+            TextField( "Text", text: $appController.text )
+                .textFieldStyle( .roundedBorder )
         }
     }
 }
